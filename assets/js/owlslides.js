2d02eb6adc493ed69788c1885b345ec5911d81cd
Raymond Wang <rw5614@gmail.com>
2020-01-14 10:20:32 -0800

    Forms

---
+++ assets/js/owlslides.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.owl-one').owlCarousel({
+        loop: true,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        items: 3,
+        navText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+    });
+  });
